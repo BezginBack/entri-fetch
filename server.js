e24cca3b97d17e63991981a1e2ab76f1d49d6cb2
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-06-30 13:04:56 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var http = require('http'), fs = require("fs"), request = require("request");
 var server = http.createServer(function(req, res){
    if (req.url === "/"){
     res.writeHead(200, {"content-type" : "text/plain"});
-    
-     
+    request('https://www.eksisozluk.com', function (err, response, body) {
+      if (err) res.write(err);
+      res.write(body);
+      res.end(); 
+    });
    }
 });  
 
