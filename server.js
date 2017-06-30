5ba75bec8995e56f7ca3d06306954273bafae36d
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-06-30 13:46:06 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var http = require('http'), fs = require("fs"), request = require("request");
+var express = require('express'), app = express();
+
+app.use(express.static(__dirname + 'public'));
 
 var server = http.createServer(function(req, res){
    if (req.url === "/"){
-    res.writeHead(200, {"content-type" : "text/plain"});
-    request('https://www.eksisozluk.com', function (err, response, body) {
-      if (err) res.write(err);
-      res.write(body);
-      res.end(); 
-    });
+    res.writeHead(200, {"content-type" : "text/html"});
+    fs.createReadStream("views/index.html").pipe(res);
    } else {
      res.writeHead(200, {"content-type" : "text/plain"});
      request('https://www.eksisozluk.com' + req.url, function (err, response, body) {
