c83a15132313d94fb3f83231bb150d9fd1d5639d
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-06-30 12:53:54 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -1,18 +1,1 @@
 
-
-
-$(document).ready(function(){
-   
-  $(".flick").on("click", function(){
-   
-  var request = require('request');
-request('https://www.eksisozluk.com/fi', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  alert('body: ' + body); // Print the HTML for the Google homepage.
-});
-    
-    
-    
-  });
-});
