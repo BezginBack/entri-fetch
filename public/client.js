921fcf3ea1261e544a27cdc04499925fd7b19362
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-06-30 06:55:28 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -28,47 +28,21 @@ $(function() {
 $(document).ready(function(){
    
   $(".flick").on("click", function(){
-    $.ajax({
-
-  // The 'type' property sets the HTTP method.
-  // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-  type: 'GET',
-
-  // The URL to make the request to.
-  url: 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html',
-
-  // The 'contentType' property sets the 'Content-Type' header.
-  // The JQuery default for this property is
-  // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-  // a preflight. If you set this value to anything other than
-  // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-  // you will trigger a preflight request.
-  contentType: 'text/plain',
-
-  xhrFields: {
-    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-    // This can be used to set the 'withCredentials' property.
-    // Set the value to 'true' if you'd like to pass cookies to the server.
-    // If this is enabled, your server must respond with the header
-    // 'Access-Control-Allow-Credentials: true'.
-    withCredentials: false
-  },
-
-  headers: {
-    // Set any custom headers here.
-    // If you set any non-simple headers, your server must include these
-    // headers in the 'Access-Control-Allow-Headers' response header.
-  },
-
-  success: function(r) {
-   alert(r.resp);
-  },
-
-  error: function(e) {
-  alert(e);
-  }
-    
-  });
+        var result = fetch("https://www.eksisozluk.com/", {
+      mode: "no-cors",
+      method: "get"
+    });
+    result
+      .then(function(response) {
+        alert("response " + response);
+        return response.getHeader();
+      })
+      .then(function(text) {
+        alert("got text " + text);
+      })
+      .catch(function(ex) {
+        alert("failed " + ex);
+      });
     
     
     
