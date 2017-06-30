278b92d6d747744bc56161c0f9e600fba63aaf3f
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-06-30 06:25:21 +0000

    Checkpoint

---
+++ cool-file.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+  
+  $(".click").on("click",function(){
+  
+  $.ajax({
+
+  // The 'type' property sets the HTTP method.
+  // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+  type: 'GET',
+
+  // The URL to make the request to.
+  url: 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html',
+
+  // The 'contentType' property sets the 'Content-Type' header.
+  // The JQuery default for this property is
+  // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
+  // a preflight. If you set this value to anything other than
+  // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
+  // you will trigger a preflight request.
+  contentType: 'text/plain',
+
+  xhrFields: {
+    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+    // This can be used to set the 'withCredentials' property.
+    // Set the value to 'true' if you'd like to pass cookies to the server.
+    // If this is enabled, your server must respond with the header
+    // 'Access-Control-Allow-Credentials: true'.
+    withCredentials: false
+  },
+
+  headers: {
+    // Set any custom headers here.
+    // If you set any non-simple headers, your server must include these
+    // headers in the 'Access-Control-Allow-Headers' response header.
+  },
+
+  success: function(r) {
+   alert(r);
+  },
+
+  error: function(e) {
+  alert(e);
+  }
+    
+  });
+    
+    
+});
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+});
