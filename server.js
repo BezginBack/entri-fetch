bfb9404848081547b48b8255d55d60cb29c9445f
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-07-03 07:59:05 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 var express = require('express');
 var app = express();
 var request = require("request");
-var url = require("url");
 
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  if(req.query.search){
+    request('https://www.eksisozluk.com/' + req.query.search, function (err, page, body) {
+      if (err) res.write(err);
+      res.writeHead(200, {"content-type" : "text/plain"});
+      res.write(body);
+      res.end();
+    });
+  } else {
+    res.sendFile(__dirname + '/views/index.html');
+  }
 });
-
-app.get(/\w/, function (req, res) {
-    res.writeHead(200, {"content-type" : "text/plain"});
-    if(req.query.search){
-      request('https://www.eksisozluk.com/'+req.query.search, function (err, page, body) {
-        if (err) res.write(err);
-        res.write(body);
-        res.end();
-      });
-    }    
-});
-
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
