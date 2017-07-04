7ba3a1c49641df7cfad1118998f9d0a2c9a4a801
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-07-04 14:35:06 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -9,24 +9,28 @@ function parseIt(url, callback){
   var data = "";
   request(url, function (err, page, body) {
     if (!err && page.statusCode == 200) {
-      var $ = cheerio.load(body);      
-      var url = page.request.uri.href;
-      var pageCounter = parseInt($(".pager").eq(0).data("pagecount"));
-      var q = async.queue(function (task, done) {
-        request(task.url, function (err, page, body2){
-          var $ = cheerio.load(body2);
-          for(var j = 0 ; j < $(".entry-date").get().length; j++){
-            data = "<div>" + task.id + " ~ " + $(".entry-author").eq(j).text() + " ~ " + $(".entry-date").eq(j).text() + "</div>";
-          }
-          done();
-          callback(null, data);
-        }); 
-      }, 1);
-      for(var i = 1; i <= pageCounter; i++) {
-        q.push({url: url+"?p="+i, id: i});
-      }    
+      var $ = cheerio.load(body);
+      if($("#topic").data("not-found") == "true"){
+        callback(null, "err");
+      } else {
+        var url = page.request.uri.href;
+        var pageCounter = parseInt($(".pager").eq(0).data("pagecount"));
+        var q = async.queue(function (task, done) {
+          request(task.url, function (err, page, body2){
+            var $ = cheerio.load(body2);
+            for(var j = 0 ; j < $(".entry-date").get().length; j++){
+              data += "<div>" + task.id + " ~ " + $(".entry-author").eq(j).text() + " ~ " + $(".entry-date").eq(j).text() + "</div>";
+            }
+            done();
+            callback(null, data);
+          }); 
+        }, 1);
+        for(var i = 1; i <= pageCounter; i++) {
+          q.push({url: url+"?p="+i, id: i});
+        }
+      }
     } else {
-      callback(null, "error or bad search");
+      callback(null, "err");
     }
   });
 }
@@ -41,11 +45,9 @@ app.get("/", function (req, res) {
     res.writeHead(200, {"content-type" : "text/html"});
     parseIt(url, function(err, data){
       if(err) res.end(err);
-      //for(var i = 0; i < data.length; i++){
-        res.write(data);  
-      //}
+      if(data == "err") res.end("error or bad search");
+      res.write(data);  
     });
-    //res.end();
   } else {
     res.sendFile(__dirname + '/views/index.html');
   }
