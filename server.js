fa2d9053c560d8d5a5a2e768b43c2a3f1a56ba10
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-07-04 07:04:31 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,33 +4,19 @@ var request = require("request");
 var jsdom = require("jsdom");
 var { JSDOM } = jsdom;
 
-function parseIt(q){
-  var html = "";
-  request('https://eksisozluk.com/', function (err, page, body) {
-    if (err) return err;
-    /*var dom = new JSDOM(body);
-    var doc = dom.window.document;
-    var url = page.request.uri.href;
-    html += "<div>" + url + "</div>";
-    var pageCounter = doc.getElementsByClassName("pager")[0].getAttribute("data-pagecount");
-    html += "<div>" + pageCounter + "</div>";
-    var nodeList = doc.getElementsByClassName("entry-date");
-    for(var j = 0 ; j < nodeList.length; j++){
-      html += "<div>" + nodeList[j].innerHTML + "</div>";
-    }*/
-    html = body;
-    return html;
-  });
-}
-
-var options = {
-  url: 'https://eksisozluk.com/'
-};
-
-function callback(err, page, body) {
-  if (!err && page.statusCode == 200) {
-    return body.title;
-  }
+function parseIt(body, page){
+  var data = "";
+  var dom = new JSDOM(body);
+  var doc = dom.window.document;
+  var url = page.request.uri.href;
+  data += "<div>" + url + "</div>";
+  var pageCounter = doc.getElementsByClassName("pager")[0].getAttribute("data-pagecount");
+  data += "<div>" + pageCounter + "</div>";
+  /*var nodeList = doc.getElementsByClassName("entry-date");
+  for(var j = 0 ; j < nodeList.length; j++){
+    html += "<div>" + nodeList[j].innerHTML + "</div>";
+  }*/
+  return data;
 }
 
 app.use(express.static('public'));
@@ -38,8 +24,12 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   if(req.query.search){
     var q = req.query.search;
+    var data = "";
+    request('https://eksisozluk.com/', function (err, page, body) {
+       data = parseIt(body, page);
+    });
     res.writeHead(200, {"content-type" : "text/html"});
-    res.write(" " + request(options, callback));
+    res.write(" " + data);
     res.end();
   } else {
     res.sendFile(__dirname + '/views/index.html');
