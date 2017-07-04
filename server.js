07d65327f517b4d9edb94ffad9d395863e160062
BezginBack <ibrahimerdemkalkan@gmail.com>
2017-07-04 13:36:22 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-var jsdom = require("jsdom");
-var { JSDOM } = jsdom;
 var async = require("async");
 var cheerio = require("cheerio");
 
@@ -13,19 +11,18 @@ function parseIt(url, callback){
     if (!err && page.statusCode == 200) {
       var $ = cheerio.load(body);
       if ($("#topic")){
-        if($("#topic").attr("data-not-found") == null){
+        if($("#topic").data("not-found") == null){
           var url = page.request.uri.href;
           //data += "<div>" + url + "</div>";
-          if(doc.getElementsByClassName("pager")[0]){
-            var pageCounter = doc.getElementsByClassName("pager")[0].getAttribute("data-pagecount");
+          if($(".pager")[0]){
+            var pageCounter = $(".pager").data("pagecount");
             //data += "<div>" + pageCounter + "</div>";
           }
           var q = async.queue(function (task, done) {
             request(task.url, function (err, page, body2){
-              var dom2 = new JSDOM(body2);
-              var doc2 = dom2.window.document;
-              var nodeList2 = doc2.getElementsByClassName("entry-date");
-              var nodeList3 = doc2.getElementsByClassName("entry-author");
+              var $ = cheerio.load(body2);
+              var nodeList2 = $(".entry-date");
+              var nodeList3 = $(".entry-author");
               for(var j = 0 ; j < nodeList2.length; j++){
                   data.push("<div>" + task.id + " ~ "+ nodeList3[j].innerHTML + " ~ " + nodeList2[j].innerHTML + "</div>");
               }
