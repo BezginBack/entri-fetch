59514dfb529b55aeb8a627a791d7118d381983d3
bezginback <ibrahimerdemkalkan@gmail.com>
2018-07-13 21:18:20 +0000

    🚿🔫 Checkpoint ./models/parser.js:365129/214

--- models/parser.js
+++ models/parser.js
@@ -2,7 +2,7 @@ var cheerio = require("cheerio");
 
 exports.parseHtml = function(body, callback){
     var $ = cheerio.load(body);
-    if($("#topic").data("not-found") == "true"){
+    if( $("#topic").eq(0).data("not-found") == true ){
       callback('no entry');
     } else {
       var pageCounter = $(".pager").eq(0).data("pagecount");
