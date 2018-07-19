0a4daf6a245c0bc409ce5877da001bc1324ba79a
bezginback <ibrahimerdemkalkan@gmail.com>
2018-07-19 16:38:15 +0000

    ⚓️👗 Checkpoint ./server.js:365129/113 ./views/index.pug:365129/56

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.route("/entries")
     //var post = {};
     for(var j = 0 ; j < $(".entry-date").get().length; j++){
       //post['entry' + j] = $(".entry-author").eq(j).text() + " - " + $(".entry-date").eq(j).text();
-      res.write('info');
+      res.write('entry' + j + ":" + $(".entry-author").eq(j).text() + " - " + $(".entry-date").eq(j).text());
     }
     res.end();
   });          
