1403c287d49d1656e40d53a30082c012281f8812
bezginback <ibrahimerdemkalkan@gmail.com>
2018-07-22 18:49:02 +0000

    🍤👲 Checkpoint ./controllers/ajax.js:365129/2623 ./server.js:365129/74

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -16,34 +16,27 @@ $('document').ready(function(){
   };
   
   $('.continue').on('click', function(){
-    /*var data = {
-      'dataPagecounter' : $('.continue').data('pagecounter'),
-      'dataPagetitle' : $('.continue').data('title'),
-      'dataPageid' : $('.continue').data('id')
-    };
-    var url = appUrl + '/entries';
-    ajaxFunction(url, data, waitFunction, function(err){
-      console.log(err);
-    }, function(res){
-      console.log(res);
-      $('.result').html(res);
-    });*/
+    $('.result').empty();
     var p = $('.continue').data('pagecounter');
-    var interval = setInterval(function(str1, str2) {
-  console.log(str1 + " " + str2);
-}, 1000, "Hello.", "How are you?");
-    var data = {
-      'url' : $('.continue').data('title') + "--" + $('.continue').data('id') + "?p=" + p
-    };
     var url = appUrl + '/entries';
-    ajaxFunction(url, data, waitFunction, function(err){
-      console.log(err);
-    }, function(res){
-      console.log(res);
-      $('.result').empty();
-      $('.result').append(res);
-      $('.result').append("<a href='" + appUrl + "'>mainpage</a>");
-    });
+    alert(p);
+    var interval = setInterval(function() {
+      if(p > 0) {
+        var data = {
+          'url' : $('.continue').data('title') + "--" + $('.continue').data('id') + "?p=" + p
+        };
+        p--;
+        ajaxFunction(url, data, waitFunction, function(err){
+          console.log(err);
+        }, function(res){
+          console.log(res);
+          $('.result').append(res);
+        });
+      } else {
+        $('.result').append("<a href='" + appUrl + "'>mainpage</a>");
+        clearInterval(interval);
+      }
+    }, 5000);
   });
 
   $('.mainpage').on('click', function(){
