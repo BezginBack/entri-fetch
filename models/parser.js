bfbe07f1a2160969fd8bdf99c842f282ee19afc1
bezginback <ibrahimerdemkalkan@gmail.com>
2018-07-24 16:48:08 +0000

    👫🎩 Checkpoint ./models/parser.js:365129/50

--- models/parser.js
+++ models/parser.js
@@ -28,7 +28,10 @@ exports.getInfo = function(url, callback){
   request(url, function (err, page, body){
     var $ = cheerio.load(body);
     for(var j = 0 ; j < $(".entry-date").get().length; j++){
-      callback("<span class='col-sm-6'>" + $(".entry-author").eq(j).text() + "</span><span class='col-sm-2'></span><span class='col-sm-4'>" + $(".entry-date").eq(j).text() + "</span>");
+      callback("<span class='col-sm-6'>" + 
+               $(".entry-author").eq(j).text() + 
+               "</span><span class='col-sm-2'></span><span class='col-sm-4'>" + 
+               $(".entry-date").eq(j).text() + "</span>");
     }
     callback('end');
   });
