c95aa4ca6436a523b0cf182f8756eecee0b3547a
erdem <ibrahimerdemkalkan@gmail.com>
2018-09-15 20:31:14 +0000

    general revision

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -1,62 +1,76 @@
 var appUrl = window.location.origin;
-$('document').ready(function(){
-  var waitFunction = function(){
-    //$('.wait').css({display: 'block'});
-    $('.wait').fadeIn();
-      var id = setInterval(function() {
-        if($('.wait').css('display') == 'block'){
-          $('.wait').animate({opacity: '0.1'}, 1000);
-          $('.wait').animate({opacity: '0.9'}, 1000);
-        } else {
-          clearInterval(id);
-        }
-      }, 1000);    
-  };
-  
-  var ajaxFunction = function(url, data, wait, err, suc){
-    $.ajax({
-      type: 'post',
-      url : url,
-      data : data,
-      beforeSend : wait,
-      error : err,
-      success : suc
-    });
-  };
-  
-  $('.continue').on('click', function(){
-    var p = $('.continue').data('pagecounter');
-    var t = p;
-    var titleId = $('.continue').data('title') + "--" + $('.continue').data('id')
-    var url = appUrl + '/entries';
-    $('.data').empty();
-    $('.result').append(
-      "<div><span class='col-sm-1 col-lg-1'>Num</span>" +
-      "<span class='col-sm-5 col-lg-5'>-Author</span>" +
-      "<span class='col-sm-1 col-lg-1'>-Fav</span>" +
-      "<span class='col-sm-5 col-lg-5'>-Date</span></div>"
-    );
-    $('title').html('EksiEntries | Result');
-    var interval = setInterval(function() {
-      if(p > 0) {
-        var data = {
-          'url' : titleId + "?p=" + (t - p + 1)
-        };
-        p--;
-        ajaxFunction(url, data, waitFunction, function(err){
-          console.log(err);
-        }, function(res){
-          $('.result').append(res);
-        });
-      } else {
-        $('.result').append("<div class='row text-center'><a href='" + appUrl + "'>Mainpage</a></div>");
-        $('.wait').css({display: 'none'});
-        clearInterval(interval);
-      }
-    }, 5000);
-  });
 
-  $('.mainpage').on('click', function(){
-    document.location.href = appUrl;
-  });
+$('document').ready(function(){
+    var waitFunction = function(){
+        $('.wait').fadeIn();
+        var id = setInterval(function() {
+            if($('.wait').css('display') == 'block'){
+                $('.wait').animate({opacity: '0.1'}, 1000);
+                $('.wait').animate({opacity: '0.9'}, 1000);
+            } else {
+                clearInterval(id);
+            }
+        }, 1000);    
+    };
+    
+    var errFunction = function(err){
+        console.log(err);
+    };
+    
+    var successFunction = function(res){
+        var block = "";
+        for (var i = 0; i < res.length; i++ ) {
+            block += "<div><span class='col-sm-1 col-lg-1'>" + res[i].num + "</span>" +
+                    "<span class='col-sm-5 col-lg-5'>" + res[i].author + "</span>" +
+                    "<span class='col-sm-1 col-lg-1'>" + res[i].favor + "</span>" +
+                    "<span class='col-sm-5 col-lg-5'>" + res[i].date + "</span></div>";
+        }
+        $('.result').append(block);
+    };
+  
+    var ajaxFunction = function(url, data, err, suc){
+        $.ajax({
+            type: 'get',
+            url : url,
+            data : data,
+            error : err,
+            success : suc
+        });
+    };
+    
+    var titleBar = "<div><span class='col-sm-1 col-lg-1'>Num</span>" +
+            "<span class='col-sm-5 col-lg-5'>Author</span>" +
+            "<span class='col-sm-1 col-lg-1'>Fav</span>" +
+            "<span class='col-sm-5 col-lg-5'>Date</span></div>";
+    
+    var homeLink = "<div class='row text-center'><a href='" + appUrl + "'>Mainpage</a></div>";
+  
+    $('.continue').on('click', function(){
+        waitFunction();
+        var p = $('.continue').data('pagecounter');
+        var t = p;
+        var titleId = $('.continue').data('title') + "--" + $('.continue').data('id');
+        var url = appUrl + '/api/entries';
+        $('.data').empty();
+        $('.result').append(titleBar);
+        $('title').html('EksiEntries | Result');
+        var interval = setInterval(function() {
+            if(p > 0) {
+                var data = {
+                    'titleId' : titleId,
+                    'p' : (t - p + 1)
+                };
+                p--;
+                ajaxFunction(url, data, errFunction, successFunction);
+            } else {
+                $('.result').append(homeLink);
+                $('.wait').css({display: 'none'});
+                clearInterval(interval);
+            }
+        }, 5000);
+    });
+
+    $('.mainpage').on('click', function(){
+        document.location.href = appUrl;
+    });
 });
--- server.js
+++ server.js
@@ -12,36 +12,30 @@ app.use('/views', express.static(__dirname + '/views'));
 app.use('/controllers', express.static(__dirname + '/controllers'));
 app.set('view engine', 'pug')
 
-app.route("/")
-  .get(function (req, res) {
-  if(req.query.search){
-    var q = req.query.search; 
-    parser.getData(url + q, function(data){
-      res.render('index', {data: data});
-    });
-  } else {
-    var data = {
-      'title': 'EksiEntries | Mainpage',
-    };
-    res.render('index', {'data' : data});
-  }
-  });
-
-app.route("/entries")
-  .post(function (req, res) {
-  var newUrl = url + req.body.url;
-  parser.getInfo(newUrl, function(info){
-    if(info == 'end') {
-      res.end();
+app.route("/").get(function (req, res) {
+    if(req.query.search){
+        var q = req.query.search; 
+        parser.getData(url + q, function(data){
+          res.render('index', {data: data});
+        });
     } else {
-      res.write(info);
+        var data = {
+          'title': 'EksiEntries | Mainpage',
+        };
+        res.render('index', {'data' : data});
     }
-  });
+});
+
+app.route("/api/entries").get(function (req, res) {
+    var newUrl = url + req.query.titleId + "/?p=" + req.query.p;
+    parser.getInfo(newUrl, function(info){
+        res.send(info);
+    });
 });
 
 app.listen(process.env.PORT, function () {
-  var date = new Date(Date.now());
-  var time = date.toLocaleTimeString('en-US', { hour12: false });
-  var day = date.toDateString();
-  console.log('Server listening :\n', 'Port:', process.env.PORT, 'Time :', day + ' ' + time);
+    var date = new Date(Date.now());
+    var time = date.toLocaleTimeString('en-US', { hour12: false });
+    var day = date.toDateString();
+    console.log('Server listening :\n', 'Port:', process.env.PORT, 'Time :', day + ' ' + time);
 });
